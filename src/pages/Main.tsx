726dfa00f72d00ebb870c35def7a9c9b7b209539
SaiKisho <diegosferas@gmail.com>
2023-07-16 17:36:52 -0500

    Project init
    set up some folders and structure

---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import { StandardButton } from "@/ui/atoms"
+
+export const Main = () => {
+    return (
+        <div>
+            Main
+            <StandardButton />
+        </div>
+    )
+}
+
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { defineConfig } from 'vite'
+import * as path from 'path'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+})
