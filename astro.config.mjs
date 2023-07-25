c0dc20fc9b6cc2113f32294dc11583f94e420681
SaiKisho <diegosferas@gmail.com>
2023-07-25 17:51:54 -0500

    🧪github pages?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://Hinkku-Company.github.io',
+  base: '/landing-page',
 });
