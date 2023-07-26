714befea655718fa91d001c351226f449987c343
SaiKisho <diegosferas@gmail.com>
2023-07-26 14:29:16 -0500

    💄Carousel Dekstop

--- src/components/React/molecules/SlideContent/Carousel/Carousel.tsx
+++ src/components/React/molecules/SlideContent/Carousel/Carousel.tsx
@@ -17,7 +17,8 @@ export const Carousel = () => {
 
     return (
         <div className='grid place-items-center gap-2 relative'>
-            <div className='grid grid-flow-col place-items-center h-[20rem]'>
+            {/* Mobile */}
+            <div className='grid grid-flow-col place-items-center h-[20rem] md:hidden'>
                 <LeftArrowIcon SvgClassName='col-span-1' onClick={prevSlide} />
                 <div className='col-span-1'>
                     {
@@ -35,7 +36,22 @@ export const Carousel = () => {
                 <RightArrowIcon SvgClassName='col-span-1' onClick={nextSlide} />
             </div>
 
-            <span className="carousel-indicators">
+            {/* Desktop */}
+            <div className='hidden md:grid grid-cols-3 gap-6'>
+                {
+                    slideData.map(({ Icon, text, title }, index) => (
+                        <li className={`item-slide-d mw-[26rem] grid place-items-center col-span-1 text-center`} key={index}>
+                            <span className='flex items-center justify-center bg-[--primary] w-[100px] h-[100px] rounded-full'>
+                                {Icon ? <Icon SvgClassName='icon-white icon-slide-d' /> : null}
+                            </span>
+                            <h3 className='text-[--color-title]'>{title}</h3>
+                            <p className='text-[--color-text] self-start mt-[1rem] px-2 lg:px-4'>{text}</p>
+                        </li>
+                    ))
+                }
+            </div>
+
+            <span className="carousel-indicators md:hidden">
                 {
                     slideData.map((_, idx: number) =>
                         <button
