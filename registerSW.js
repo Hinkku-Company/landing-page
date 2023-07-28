696e8b05c24dc53bc9b4c5abc262e07af56caa7c
RuxTsuki <RuxTsuki@users.noreply.github.com>
2023-07-28 17:50:14 +0000

    deploy: 4eb54e66fea31bd320d802b3a71eadb0f26f30a3

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/landing-pagesw.js', { scope: '/landing-page' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/landing-page/sw.js', { scope: '/landing-page/' })})}
